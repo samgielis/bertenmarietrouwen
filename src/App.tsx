11c8efc56613e0794175c19498749a6233f54053
Sam Gielis <sam.gielis@trustedfamily.net>
2022-01-08 15:14:55 +0100

    Add fonts & bg pattern
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
   VStack,
-  Code,
-  Grid,
-  theme,
+  Heading,
+  Container,
+  Text,
+  Flex,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { theme } from "./theme"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+    <Flex h="100vh" justify={"center"} align={"center"}>
+      
+    <Container textAlign="center">
+        <VStack spacing={32}>
+          <VStack  spacing={6} letterSpacing={"1em"} textTransform={"uppercase"} >
+            <Heading fontWeight={"400"} size={"4xl"}>
+              Save
+            </Heading>
+            <Heading fontWeight={"400"} size={"lg"} fontStyle={"italic"}>
+              the
+            </Heading>
+            <Heading fontWeight={"400"} size={"4xl"}>
+              date
+            </Heading>
+          </VStack>
+          <VStack spacing={12}>
+            <Heading fontWeight={"400"}  letterSpacing={".3em"} size="md" textTransform={"uppercase"} fontSize={"4xl"}>
+              20.08.2022
+            </Heading>
+            <Text textTransform={"uppercase"} fontWeight="900">
+              Bert en Marie
+            </Text>
+          </VStack>
         </VStack>
-      </Grid>
-    </Box>
+    </Container>
+    </Flex>
   </ChakraProvider>
 )
