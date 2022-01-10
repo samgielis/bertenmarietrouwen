a77306280c5bc0cea60fad3eef993c82c17a60b0
Sam Gielis <sam.gielis@trustedfamily.net>
2022-01-10 19:03:43 +0100

    Design SibscribeEmail component
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

---
+++ src/components/SubscribeEmail.tsx
@@ -0,0 +1,76 @@
+import { Stack, FormControl, Text, Input, FormErrorMessage, FormHelperText, Button, useToast, Box, Heading } from "@chakra-ui/react";
+import { useForm } from "react-hook-form"
+
+const SUBSCRIBE_NEWSLETTER_ENDPOINT = '/.netlify/functions/subscribeToNewsletter';
+
+export const SubscribeEmail = () => {
+    const toast = useToast();
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting }
+    } = useForm();
+
+    function onSubmit(values: { email: string }) {
+        const handleError = () => {
+            toast({
+                title: "Er ging iets mis.",
+                description: "Probeer het later nog eens of contacteer ons op info@bertenmarietrouwen.be.",
+                status: "error",
+                isClosable: true,
+            });
+        }
+        return new Promise((resolve, reject) => {
+            fetch(SUBSCRIBE_NEWSLETTER_ENDPOINT, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            }).then((response) => {
+                if (response.status !== 200) {
+                    handleError();
+                    reject();
+                } else {
+                    toast({
+                        title: "Ingeschreven.",
+                        description: "Je bent succesvol ingeschreven op de nieuwsbrief.",
+                        status: "success",
+                        isClosable: true,
+                    });
+                    resolve(undefined);
+                }
+            }).catch((e) => {
+                handleError();
+                reject();
+            })
+        });
+    }
+
+    return <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={6}>
+        <Text fontSize="md" fontWeight={"bold"} textTransform="uppercase">Hou me op de hoogte</Text>
+            <FormControl id="email" isRequired>
+                <Input type="email"
+                    placeholder="voorbeeld@gmail.com"
+                    {...register("email", {
+                        required: "E-mail is verplicht",
+                    })}
+                    borderRadius={0}
+                    />
+                <FormErrorMessage>
+                    {errors.email && errors.email.message}
+                </FormErrorMessage>
+                <FormHelperText  opacity={.8} fontStyle={"italic"} >Van zodra we alle info hebben krijg je op dit adres een update.</FormHelperText>
+
+            </FormControl>
+            <Box>
+            <Button color="white" fontStyle="italic" borderRadius={0} bgColor="black" colorScheme="gray" border="1px solid transparent" _hover={{bgColor: "white", color: "black", border: "1px solid black"}} mr={3} isLoading={isSubmitting} type="submit">
+                Inschrijven
+            </Button>
+
+            </Box>
+        </Stack>
+
+    </form>
+}
