2a5fc7092969b545f9afe906561a85b51d6f78a9
Sam Gielis <sam.gielis@trustedfamily.net>
2022-09-29 22:06:25 +0200

    Add alt to img
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/screens/ThankYou.tsx
+++ src/screens/ThankYou.tsx
@@ -3,7 +3,7 @@ import { Stack, Heading, Box } from "@chakra-ui/react";
 export const ThankYou = () => (
     <Stack spacing={20}>
         <Box borderRadius='3xl' overflow={'hidden'}>
-            <img src='thx.jpeg' />
+            <img src='thx.jpeg' alt='Bedankt om er bij te zijn, jammer genoeg laadt deze mooie foto niet.' />
         </Box>
         <Stack>
             <Heading sixe='2xl' letterSpacing={2}>Bedankt om er bij te zijn.</Heading>
