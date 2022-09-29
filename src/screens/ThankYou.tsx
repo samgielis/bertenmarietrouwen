857866415b811e24d819e849e07a644900ef7abb
Sam Gielis <sam.gielis@trustedfamily.net>
2022-09-29 21:57:34 +0200

    Replace site with thank you
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

---
+++ src/screens/ThankYou.tsx
@@ -0,0 +1,13 @@
+import { Stack, Heading, Box } from "@chakra-ui/react";
+
+export const ThankYou = () => (
+    <Stack spacing={20}>
+        <Box borderRadius='3xl' overflow={'hidden'}>
+            <img src='thx.jpeg' />
+        </Box>
+        <Stack>
+            <Heading sixe='2xl' letterSpacing={2}>Bedankt om er bij te zijn.</Heading>
+            <Heading size="xl" letterSpacing={4} fontWeight={400}>Bert & Marie</Heading>
+        </Stack>
+    </Stack>
+)
